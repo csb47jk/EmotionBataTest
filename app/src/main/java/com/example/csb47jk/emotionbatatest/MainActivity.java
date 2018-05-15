e2486721c38ffa46d302133a40aa800c031d9dd8
Fangyufa <csb47jk@gmail.com>
2018-05-16 02:42:54 +0800

    實作必要的abstract

--- app/src/main/java/com/example/csb47jk/emotionbatatest/MainActivity.java
+++ app/src/main/java/com/example/csb47jk/emotionbatatest/MainActivity.java
@@ -2,11 +2,55 @@ package com.example.csb47jk.emotionbatatest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.widget.Toast;
 
+import com.affectiva.android.affdex.sdk.Frame;
+import com.affectiva.android.affdex.sdk.detector.CameraDetector;
+import com.affectiva.android.affdex.sdk.detector.Detector;
+import com.affectiva.android.affdex.sdk.detector.Face;
+
+import java.util.List;
+
+//Activity   //AppCompatActivity
+public class MainActivity extends AppCompatActivity  implements Detector.ImageListener, CameraDetector.CameraEventListener  {
+
+    SurfaceView cameraPreview;
+    CameraDetector detector;
+
+    int previewWidth = 0;
+    int previewHeight = 0;
 
 
-public class MainActivity extends AppCompatActivity {
+
+
+    // 切換手機前後鏡頭
+    void switchCamera(CameraDetector.CameraType type) {
+        detector.setCameraType(type);
+    }
+
+    //@SuppressWarnings("SuspiciousNameCombination")
+    @Override
+    public void onCameraSizeSelected(int width, int height, Frame.ROTATE rotate){
+                 //  CameraDetector.CameraEventListener  必須實作 (abstract)  onCameraSizeSelected
+        if (rotate == Frame.ROTATE.BY_90_CCW || rotate == Frame.ROTATE.BY_90_CW) {
+            previewWidth = height;
+            previewHeight = width;
+        } else {
+            previewHeight = height;
+            previewWidth = width;
+        }
+        cameraPreview.requestLayout();
+
+    }
+
+
+    @Override
+    public void onImageResults(List<Face> list, Frame frame, float v){
+                // Detector.ImageListener 必須實作 (abstract)   onImageResults
+
+    }
+
 
 
 
